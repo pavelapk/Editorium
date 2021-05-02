59c8050372ae4eeb9abd4862a20684049bfa0465
pavelapk <pepdj@mail.ru>
2021-05-02 16:42:25 +0700

    rename Canvas to Viewport

--- app/src/main/java/ru/imageella/editorium/interfaces/Viewport.kt
+++ app/src/main/java/ru/imageella/editorium/interfaces/Viewport.kt
@@ -2,7 +2,7 @@ package ru.imageella.editorium.interfaces
 
 import android.graphics.Bitmap
 
-interface Canvas {
+interface Viewport {
     fun setBitmap(bitmap: Bitmap)
     fun previewRotate(angle: Float)
 }
